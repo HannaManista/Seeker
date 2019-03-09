bf83673fdf49cf6fbd82a8e20eddd99ecf409621
Krzysztof Szewczyk <krzysieksz96@gmail.com>
2019-03-09 16:39:17 +0100

    main

--- src/com/ahks/seekerApp/Seeker.java
+++ src/com/ahks/seekerApp/Seeker.java
@@ -1,4 +1,17 @@
 package com.ahks.seekerApp;
 
+import javax.swing.*;
+
 public class Seeker {
+    private JPanel panelMain;
+    private JLabel label1;
+
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Seeker");
+        frame.setContentPane(new Seeker().panelMain);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
