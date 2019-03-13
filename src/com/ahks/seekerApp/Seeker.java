03ad49f7ea427ba9d4422e77154e27b9616d47f9
krzysiek <krzysieksz96@gmail.com>
2019-03-13 11:32:22 +0100

    update

--- src/com/ahks/seekerApp/Seeker.java
+++ src/com/ahks/seekerApp/Seeker.java
@@ -1,4 +1,13 @@
 package com.ahks.seekerApp;
 
+import javax.swing.*;
+
 public class Seeker {
+    private JButton button1;
+    private JLabel label1;
+    private JPanel panel1;
+
+    public static void main(String[] args) {
+        System.out.println("xD");
+    }
 }
